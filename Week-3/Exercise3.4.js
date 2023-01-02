93e12138e34ce74bcfb3a476461b8a11043814b6
Sk Ibrar Ahmed <skibrara@gmail.com>
2023-01-02 19:29:47 +0530

    comments added

--- Week-3/Exercise3.4.js
+++ Week-3/Exercise3.4.js
@@ -1,6 +1,8 @@
+
+//using the concept of closure
 function createStack() {
      
-      const items = [];
+      const items = []; //items has been moved to a variable inside createStack() scope.from the outside of createStack() scope, there is no way to access or modify items array. items is now a private variable, and the stack is encapsulated: only push() and pop() method are public.
       return { 
       push(item) 
       {
@@ -14,7 +16,8 @@ function createStack() {
       const stack= createStack();
       stack.push(10);
       stack.push(5);
-      stack.pop();// => 5
+      console.log(stack.pop());// => 5
       stack.items;  // => [10]
-      //stack.items= [10,100,1000];  // Encapsulationbroken!
+      
+      
       
