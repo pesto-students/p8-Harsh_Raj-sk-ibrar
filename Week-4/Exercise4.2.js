a7d8870c91ab26d5b192032f752dc960322a2c06
Sk Ibrar Ahmed <skibrara@gmail.com>
2023-01-09 10:21:00 +0530

    Ex 4.2 added- js code

---
+++ Week-4/Exercise4.2.js
@@ -0,0 +1,25 @@
+var Person = function() {};
+
+Person.prototype.initialize = function(name, age)
+{
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.describe = function()
+{
+    return this.name + ", " + this.age + " years old.";
+}
+
+var Teacher = function() {};
+Teacher.prototype = new Person();
+
+Teacher.prototype.teach = function(subject)
+{
+    console.log(this.name + " just learned " + subject);
+}
+
+var me = new Teacher();
+
+me.initialize("Arafat", 30);
+me.teach("history");
