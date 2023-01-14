f93b6050f388cb95587e9ad53d75f904e56e2618
Sk Ibrar Ahmed <skibrara@gmail.com>
2023-01-14 07:40:35 +0530

    Ex 4.1 changed

--- Week-4/Exercise4.1.js
+++ Week-4/Exercise4.1.js
@@ -1,13 +1,18 @@
 const pro=new Promise(function(resolve, reject)
 {
-    const rNo= parseInt(math.random()*100,10);
+    const rNo= parseInt(Math.random()*100,10);
 
     setTimeout(()=>{
         if (rNo%5===0){
-        reject("promise rejected")
+        reject('promise rejected: ${rNo} ')
     }
     else{
         resolve("promise is resolved")
     }
+}, rNo*10)
 })
-})
+pro.then(console.log)
+pro.catch(console.log)
+
+// pro.then(console.log( pro.resolve))
+// pro.catch(pro.reject)
